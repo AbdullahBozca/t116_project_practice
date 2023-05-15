d08327175269509ea1c911cfecc33102aece32bb
Bozca <abdullahbozca@gmail.co>
2023-05-15 19:22:08 +0300

    5/15/2023-2

--- src/mayis08/OgrenciIslemleri.java
+++ src/mayis08/OgrenciIslemleri.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class OgrenciIslemleri {
     Scanner scan = new Scanner(System.in);
-    static ArrayList<Ogrenci> ogrenciList=new ArrayList();
+    static ArrayList<Ogrenci> ogrenciList = new ArrayList();
+
     void ogrenciMenu() {
         System.out.println("============= İŞLEMLER =============\n" +
                 "\t\t 1-EKLEME\n" +
@@ -30,10 +31,10 @@ public class OgrenciIslemleri {
                 silme();
             }
             case 'Q': {
-                Anamenu anamenu=new Anamenu();
+                Anamenu anamenu = new Anamenu();
                 anamenu.menu();
             }
-            default:{
+            default: {
                 System.out.println("HATALI GİRİŞ YAPTINIZ...");
                 ogrenciMenu();
             }
@@ -43,37 +44,36 @@ public class OgrenciIslemleri {
     private void silme() {
         //kimlik no ile silme ve arama
         System.out.print("SİLİNECEK ÖĞRENCİNİN TCNO SUNU GİRİNİZ: ");
-        String tcNo=scan.next();
-        boolean flag=false;
+        String tcNo = scan.next();
+        boolean flag = false;
         for (int i = 0; i < ogrenciList.size(); i++) {
-            if (tcNo.equals(ogrenciList.get(i).getTcNo())){
-                System.out.println("SİLİNECEK ÖĞRENCİNİN BİLGİLERİ: "+ ogrenciList.get(i).toString());
+            if (tcNo.equals(ogrenciList.get(i).getTcNo())) {
+                System.out.println("SİLİNECEK ÖĞRENCİNİN BİLGİLERİ: " + ogrenciList.get(i).toString());
                 ogrenciList.remove(i);
-                flag=true;
-                listeleme();
+                flag = true;
+                break;
             }
         }
-        if (!flag){ // bu if bloğunun çalışabilmesi için false değeri gelmeli
-            System.out.println(tcNo+" YA AİT VERİ BULMAMAKTADIR...");
+        if (!flag) { // bu if bloğunun çalışabilmesi için false değeri gelmeli
+            System.out.println(tcNo + " YA AİT VERİ BULMAMAKTADIR...");
             listeleme();
         }
-
-
+        ogrenciMenu();
     }
 
     private void arama() {
         //kimlik no ile silme ve arama
         System.out.println("ARANACAK ÖĞRENCİNİN TCNO SUNU GİRİNİZ: ");
-        String tcNo=scan.next();
-        int flag=0;
-        for (Ogrenci each: ogrenciList) {
+        String tcNo = scan.next();
+        int flag = 0;
+        for (Ogrenci each : ogrenciList) {
             if (each.getTcNo().equals(tcNo)) {
-                System.out.println("ARADIĞINIZ ÖĞRENCİ= "+ each);
-                flag=1;
+                System.out.println("ARADIĞINIZ ÖĞRENCİ= " + each);
+                flag = 1;
                 break;
             }
         }
-        if (flag==0){
+        if (flag == 0) {
             System.out.println("ARADIĞINIZ ÖĞRENCİ YOKTUR");
             ogrenciMenu();
         }
@@ -81,7 +81,7 @@ public class OgrenciIslemleri {
     }
 
     private void listeleme() {
-        for (Ogrenci each: ogrenciList) {
+        for (Ogrenci each : ogrenciList) {
             System.out.println(each);
         }
         ogrenciMenu();
@@ -91,18 +91,18 @@ public class OgrenciIslemleri {
         //Öğrenci:  Ad-Soyad,  kimlik No, yaş, numara, sınıf
         scan.nextLine();
         System.out.println("ÖĞRENCİNİN ADINI GİRİNİZ: ");
-        String ad=scan.nextLine();
+        String ad = scan.nextLine();
         System.out.println("ÖĞRENCİNİN SOYADINI GİRİNİZ: ");
-        String sad=scan.nextLine();
+        String sad = scan.nextLine();
         System.out.println("ÖĞRENCİNİN TCNO GİRİNİZ: ");
-        String tcNo=scan.nextLine();
+        String tcNo = scan.nextLine();
         System.out.println("ÖĞRENCİNİN YAŞINI GİRİNİZ: ");
-        int yas=scan.nextInt();
+        int yas = scan.nextInt();
         System.out.println("ÖĞRENCİNİN NUMARA GİRİNİZ: ");
-        int numara=scan.nextInt();
+        int numara = scan.nextInt();
         System.out.println("ÖĞRENCİNİN SINIFINI GİRİNİZ: ");
-        String sinif=scan.next();
-        Ogrenci ogrenci=new Ogrenci(ad,sad,tcNo,yas,numara,sinif);
+        String sinif = scan.next();
+        Ogrenci ogrenci = new Ogrenci(ad, sad, tcNo, yas, numara, sinif);
         ogrenciList.add(ogrenci);
 
         listeleme();
